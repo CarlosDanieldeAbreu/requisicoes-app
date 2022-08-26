afc575aae3f8fc70b771bee13cf0b65670d4dac8
CarlosDanieldeAbreu <87880996+CarlosDanieldeAbreu@users.noreply.github.com>
2022-08-26 16:34:07 -0300

    Navbar configurado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBobymRs1if1foB0sXqb5_Mg0TJ0-hvRss",
+    authDomain: "requisicoes-app-ab997.firebaseapp.com",
+    projectId: "requisicoes-app-ab997",
+    storageBucket: "requisicoes-app-ab997.appspot.com",
+    messagingSenderId: "229407980980",
+    appId: "1:229407980980:web:313c512f0aa59d496b86e1"
+  }
 };
 
 /*
