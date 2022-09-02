b9756e88f1e43e34e34069c2fbd876834502d6b2
CarlosDanieldeAbreu <87880996+CarlosDanieldeAbreu@users.noreply.github.com>
2022-09-02 16:23:27 -0300

    implementado validações em equipamnetos

--- src/app/equipamentos/equipamento.component.ts
+++ src/app/equipamentos/equipamento.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs';
 import { Equipamento } from './models/equipamento.model';
@@ -25,9 +25,9 @@ export class EquipamentoComponent implements OnInit {
     this.equipamentos$ = this.equipamentoService.selecionarTodos();
     this.form = this.fb.group({
       id: new FormControl(""),
-      nome: new FormControl(""),
-      preco: new FormControl(""),
-      data: new FormControl("")
+      nome: new FormControl("", [Validators.required, Validators.minLength(3)]),
+      preco: new FormControl("", [Validators.required]),
+      data: new FormControl("", [Validators.required])
     });
   }
 
@@ -39,15 +39,15 @@ export class EquipamentoComponent implements OnInit {
     return this.form.get("id");
   }
 
-  get nome() {
+  get nome(): AbstractControl | null {
     return this.form.get("nome");
   }
 
-  get preco() {
+  get preco(): AbstractControl | null {
     return this.form.get("preco");
   }
 
-  get data() {
+  get data(): AbstractControl | null {
     return this.form.get("data");
   }
 
@@ -63,30 +63,33 @@ export class EquipamentoComponent implements OnInit {
       let tipo: string;
       let tipo2: string;
 
-      if (!equipamento) {
-        await this.equipamentoService.inserir(this.form.value);
-        tipo = "inserido"
-        tipo2 = "Cadastro"
-      }
-      else {
-        await this.equipamentoService.editar(this.form.value);
-        tipo = "editado"
-        tipo2 = "Edição"
-      }
+      if(this.form.dirty && this.form.valid){
+        if (!equipamento) {
+          await this.equipamentoService.inserir(this.form.value);
+          tipo = "inserido";
+          tipo2 = "Cadastro";
+        }
+        else {
+          await this.equipamentoService.editar(this.form.value);
+          tipo = "editado";
+          tipo2 = "Edição";
+        }
 
-      this.toastrService.success(`O equipamento foi ${tipo} com sucesso`, `${tipo2} de equipamentos`);
+        this.toastrService.success(`O equipamento foi ${tipo} com sucesso`, `${tipo2} de equipamentos`);
+      }else
+        this.toastrService.error(`O formulário precisa ser preenchido!.`, `Cadastro de Equipamento`);
 
     } catch (error) {
       let tipo: string;
       let tipo2: string;
       if (error != "fechar" && error != "0" && error != "1") {
         if (!equipamento) {
-          tipo = "inserir"
-          tipo2 = "Cadastro"
+          tipo = "inserir";
+          tipo2 = "Cadastro";
         }
         else {
-          tipo = "editar"
-          tipo2 = "Edição"
+          tipo = "editar";
+          tipo2 = "Edição";
         }
 
         this.toastrService.error(`Houve um erro ao ${tipo} equipamento. Tente novamente`, `${tipo2} de equipamentos`);
