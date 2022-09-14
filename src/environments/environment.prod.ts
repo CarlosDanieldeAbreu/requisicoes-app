137001c4dc10d23b4615c30f897cb89c6cf615dc
CarlosDanieldeAbreu <87880996+CarlosDanieldeAbreu@users.noreply.github.com>
2022-09-14 17:16:48 -0300

    modificações

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBobymRs1if1foB0sXqb5_Mg0TJ0-hvRss",
+    authDomain: "requisicoes-app-ab997.firebaseapp.com",
+    projectId: "requisicoes-app-ab997",
+    storageBucket: "requisicoes-app-ab997.appspot.com",
+    messagingSenderId: "229407980980",
+    appId: "1:229407980980:web:313c512f0aa59d496b86e1"
+  }
 };
